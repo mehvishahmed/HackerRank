6d4fa013b6a135df48e21c8a08988d8c22c0ec38
Mehvish Ahmed <84987764+mehvishahmed@users.noreply.github.com>
2021-07-10 09:52:38 +0530

    Data Types

---
+++ 30 Days of Code/Day 1: Data Types.py
@@ -0,0 +1,10 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+int_num = input()
+double_num = input()
+str_1 = input()
+sum_int = int(int_num) + i
+double_d = float(double_num) + d
+str_2 = s + str_1
+print('{0}\n{1:0.1f}\n{2}'.format(sum_int,double_d,str_2))
